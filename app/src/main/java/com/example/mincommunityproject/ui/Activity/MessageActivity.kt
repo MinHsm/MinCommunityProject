0e63029f761cfe5d3dcc5b736f2f325c4b497caf
ming <2359848791@qq.com>
2024-05-23 21:08:28 +0800

    消息页面开始

--- app/src/main/java/com/example/mincommunityproject/ui/Activity/MessageActivity.kt
+++ app/src/main/java/com/example/mincommunityproject/ui/Activity/MessageActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mincommunityproject.R
 
-class RankingListActivity : AppCompatActivity() {
+class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ranking_list)
+        setContentView(R.layout.activity_message)
     }
 }
