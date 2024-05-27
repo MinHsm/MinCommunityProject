c624d14b97043b5378a5e9a2ad890a75e8034fc4
ming <2359848791@qq.com>
2024-05-27 11:25:11 +0800

    登录初始化

---
+++ app/src/main/java/com/example/mincommunityproject/ui/Activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mincommunityproject.ui.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mincommunityproject.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
